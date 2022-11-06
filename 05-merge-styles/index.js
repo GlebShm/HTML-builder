e942515fbc2e92322fc089782c1531186072d6a4
Gleb <glebshm@rambler.ru>
2022-11-06 15:49:06 +0300

    feat: task 5 solution added

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const path = require('path')
+
+fs.cre
+
+let stylesDir = path.join(__dirname, 'styles')
+fs.readdir(stylesDir, (err, files) => {
+  if (err) throw err
+  let arr = []
+  for (let file of files) {
+    let filePath = stylesDir + '\\' + file
+    fs.stat(filePath, (err, stats) => {
+      if (!stats.isDirectory() && path.extname(filePath) == '.css') {
+        let readStream = fs.createReadStream(filePath)
+        readStream.on('data', (chunk) => {
+          arr.push(chunk)
+        })
+        let writeStream = fs.createWriteStream(path.join(__dirname, 'project-dist', 'bundle.css'))
+        readStream.on('end', () => {
+          writeStream.write(arr.join(''))
+        })
+      }
+    })
+  }
+})
