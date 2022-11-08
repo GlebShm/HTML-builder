ec0b5fbf365476ec7d609c9fe26d56bac9a2d3b6
Gleb <glebshm@rambler.ru>
2022-11-08 21:01:30 +0300

    feat: add task 4 fix

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,12 +2,15 @@ const fs = require('fs')
 const path = require('path')
 
 let dirPath = path.join(__dirname, 'files-copy')
-fs.mkdir(dirPath, { recursive: true }, (err) => {
-  if (err) throw err
-  fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+fs.rm(dirPath, { recursive: true, force: true }, () => {
+  fs.mkdir(dirPath, { recursive: true }, (err) => {
     if (err) throw err
-    for (let file of files) {
-      fs.copyFile(path.join(__dirname, 'files', file), dirPath + '\\' + file, () => { })
-    }
+    fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+      if (err) throw err
+      for (let file of files) {
+        fs.copyFile(path.join(__dirname, 'files', file), dirPath + '\\' + file, () => { })
+      }
+    })
   })
 })
+
